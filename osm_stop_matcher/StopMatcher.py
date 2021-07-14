bc0ead055fddcdea93bbe21e20be25b9d6821ca4
Holger Bruch <hb@mfdz.de>
2021-07-14 16:16:59 +0200

    Remove locality from next/previous stop as it reduces specificity

--- osm_stop_matcher/StopMatcher.py
+++ osm_stop_matcher/StopMatcher.py
@@ -66,13 +66,19 @@ class StopMatcher():
 
 	def rank_successor_matching(self, stop, osm_stop):
 		richtung = stop["Name_Steig"]
+		ortsteil = stop['Ortsteil']
 		if richtung:
 			match = re.match('(.*)(eRtg|Ri |>|Ri\.|Rtg|Richt |Fahrtrichtung|Ri-|Ri:|Richtung|Richtg\.|FR )(.*)', richtung)
 			if match:
 				richtung = match.group(3).strip()
+				richtung = richtung.replace(ortsteil, '')
+				richtung = richtung.replace(',', '')
 			if (len(stop["Name_Steig"]) > len(richtung)):
-				similarity_next = ngram.NGram.compare(richtung, osm_stop["next_stops"],N=1)
-				similarity_prev = ngram.NGram.compare(richtung, osm_stop["prev_stops"],N=1)
+				next_stops = osm_stop["next_stops"].replace(ortsteil, '')  if osm_stop["next_stops"] else None
+				prev_stops = osm_stop["prev_stops"].replace(ortsteil, '') if osm_stop["prev_stops"] else None
+				
+				similarity_next = ngram.NGram.compare(richtung, next_stops,N=1)
+				similarity_prev = ngram.NGram.compare(richtung, prev_stops,N=1)
 				if similarity_next > 0.7 and similarity_prev < 0.5:
 					return 1
 				elif similarity_prev > 0.7 and similarity_next < 0.5:
