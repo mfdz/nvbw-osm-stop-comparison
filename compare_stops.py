485d3c43884bb84b37c384d75600671e5453a880
Holger Bruch <hb@mfdz.de>
2021-07-14 16:19:34 +0200

    Introduce arguments

--- compare_stops.py
+++ compare_stops.py
@@ -1,6 +1,7 @@
 """
 Compare OSM stops to NVBW provided stops.
 """
+import argparse
 import geojson
 
 import sys
@@ -14,45 +15,42 @@ from osm_stop_matcher.NvbwStopsImporter import NvbwStopsImporter
 from osm_stop_matcher.StopMatcher import StopMatcher
 from osm_stop_matcher.MatchResultValidator import MatchResultValidator
 from osm_stop_matcher.OsmStopsImporter import OsmStopsImporter
-		
+        
 import logging
 
 # TODO
-# instead of using coded booleans for imports, derive that from params
 # introduce some more deterministics choosing ambigous candidates as winning match
 # label matches for which an equally rated candidate exists as ambigous
 # label current ambigous match as matched_parent_stop_only
 # pre/succ currently only works for (bus) platforms...
 def main(osmfile, stops_file):
-	logging.basicConfig(filename='matching.log', filemode='w', level=logging.INFO)
-	db = spatialite.connect('stops.db')
-	db.execute("PRAGMA case_sensitive_like=ON")
-	db.row_factory = sqlite3.Row
+    logging.basicConfig(filename='matching.log', filemode='w', level=logging.INFO)
+    db = spatialite.connect('stops.db')
+    db.execute("PRAGMA case_sensitive_like=ON")
+    db.row_factory = sqlite3.Row
 
-	import_haltestellen = True	
-	import_osm = True
+    if stops_file:
+        NvbwStopsImporter(db).import_stops(stops_file)
+        print("Imported NVBW stops")
+    
+    if osmfile:
+        OsmStopsImporter(db, osm_file = osmfile)
+        print("Imported osm file")
 
-	if import_haltestellen:
-		NvbwStopsImporter(db).import_stops(stops_file)
-		print("Imported NVBW stops")
-	
-	if import_osm:
-		OsmStopsImporter(db, osm_file = osmfile)
-		print("Imported osm file")
+    StopMatcher(db).match_stops()
+    print("Matched and exported candidates")
+    MatchPicker(db).pick_matches()
+    MatchResultValidator(db).check_assertions()
+    StatisticsUpdater(db).update_match_statistics()
+    
+    db.close()
 
-	StopMatcher(db).match_stops()
-	print("Matched and exported candidates")
-	MatchPicker(db).pick_matches()
-	MatchResultValidator(db).check_assertions()
-	StatisticsUpdater(db).update_match_statistics()
-	
-	db.close()
-
-	return 0
+    return 0
 
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		print("Usage: python %s <osmfile> <stopsfile>" % sys.argv[0])
-		sys.exit(-1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', dest='osmfile', required=False, help='OpenStreetMap pbf file')
+    parser.add_argument('-s', dest='stopsfile', required=False, help='Stops file')
+    args = parser.parse_args()
 
-	exit(main(sys.argv[1], sys.argv[2]))
+    exit(main(args.osmfile, args.stopsfile))
